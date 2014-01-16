41d074c56a9b71a0766e3157b812bbb94955e3e3
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-01-16 12:12:01 -0500

    added GPL License stub to all files added full text version of GPL License

--- Daily/Event.h
+++ Daily/Event.h
@@ -6,6 +6,23 @@
 //  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
 //
 
+/* 
+    This file is part of Daily Pictures.
+
+    This programme is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This programme is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Daily Pictures.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
